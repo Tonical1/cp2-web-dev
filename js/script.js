fb23c6e1855c6ea134ccefda8323dcdd9c551de6
Bruno Biletsky <brunobiletsky@gmail.com>
2024-05-02 00:27:52 -0300

    Resolvendo parte do exercícios - Victor Freire

--- js/script.js
+++ js/script.js
@@ -44,3 +44,101 @@ function slideshow(){
     setTimeout('slideshow()', time)
 }
 slideshow();
+
+//validando login
+function validarlogin(){
+    let validar = false;
+    if(document.getElementById("nomelogin").value =="admin" && document.getElementById("senhalogin").value =="123"){ 
+        validar = true;
+    }else{
+        //Não fazer nada.
+    } return validar
+}
+function enviandologin(){
+    if(validarlogin()){
+        alert("redirecionando...")
+        window.location.replace("quiz.html")
+        
+        
+    }else{
+        alert("Usuário ou senha incorretos!")
+}}
+
+//quiz
+
+function iniciarquiz(){
+    let pontos = 0;
+    let resposta1 = prompt("Quantas rodas tem uma bike comum?")
+    if (resposta1 == "2"){
+        alert("acertou, +1 ponto!")
+        pontos = 1;
+    } else{
+        alert("Resposta incorreta!")
+    }
+    let resposta2 = prompt("Em qual ano foi inventada a bike?")
+    if (resposta2 == "1817"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1;
+    } else{
+        alert("Resposta incorreta!")
+    }
+    let resposta3 = prompt("Qual o primeiro nome da pessoa que inventou a bike?").toLowerCase()
+    console.log(resposta3)
+    if (resposta3 == "karl"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1
+    } else{
+        alert("Resposta incorreta!")
+    }
+    let resposta4 = prompt("Qual o sobrenome da pessoa que inventou a bike (haha achou que não iria perguntar)?").toLowerCase()
+    if (resposta4 == "von drais"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1
+    } else{
+        alert("Resposta incorreta!")
+    }
+    let resposta5 = prompt("Em qual ano o ciclismo se tornou esporte olímpico?")
+    if (resposta5 == "1996"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1
+    } else{
+        alert("Resposta incorreta!")
+    }
+    let resposta6 = prompt("O criador do site gosta de andar de bike (: ?").toLowerCase()
+    if (resposta6 == "sim"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1
+    } else{
+        alert("Resposta incorreta!")
+    }
+    let resposta7 = prompt("Esta gostando do quiz?")
+    if (resposta7 == "sim"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1
+    } else{
+        alert("Po, sacanagem )= ")
+    }
+    let resposta8 = prompt("Qual a cidade brasileira que mais usa bicicletas em deslocamentos diários?").toLowerCase()
+    if (resposta8 == "porto alegre"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1
+    } else{
+        alert("Resposta incorreta!")
+    }
+    let resposta9 = prompt("Qual cidade do Brasil tem maior numero de ciclofaixas?").toLowerCase()
+    if (resposta9 == "são paulo" || "sao paulo"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1
+    } else{
+        alert("Resposta incorreta!")
+    }
+    let resposta10 = prompt("Qual o nome do esporte radical em terrenos irregulares (principalmente montanhas) com bike?").toLowerCase()
+    if (resposta10 == "mountain bike"){
+        alert("acertou, +1 ponto!")
+        pontos = pontos + 1
+    } else{
+        alert("Resposta incorreta!")
+    }
+    alert(`Parabens! Você finalizou o quiz com ${pontos} pontos`)
+}
+function respostas()
